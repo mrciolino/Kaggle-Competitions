abf07f558b21b21a70f7dd270a159b4ff9069aa4
mrciolino <mrciolino@comcast.net>
2018-07-19 18:14:53 -0400

    progress

---
+++ Home Credit/AutoFeatureEngrTutorial/Will K. Tutorial.py
@@ -0,0 +1,10 @@
+import featuretools
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.size'] = 22
+import seaborn as sns
+
+import warnings
+warnings.filterwarnings('ignore')
