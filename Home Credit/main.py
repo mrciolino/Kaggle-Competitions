24ce557c34cdf344047a6fe5f6d10e22f7bd2087
Matthew Ciolino <mrciolino@Matthews-MacBook-Pro.local>
2018-07-25 17:10:17 -0400

    Implemente LightGBM into model

--- Home Credit/main.py
+++ Home Credit/main.py
@@ -1,61 +1,50 @@
-import pickle
-import warnings
-import numpy as np
 from sklearn.svm import LinearSVC
-from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold, cross_validate
 from sklearn.feature_selection import SelectKBest
-from sklearn.metrics import classification_report
-from home_credit_data_tools import data_creation_method, create_submission, save_clf_data_to_pickle
+from sklearn.metrics import classification_report, mean_squared_error
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold, cross_validate
 
+from home_credit_data_tools import data_creation_method, create_submission, save_classifier
+import lightgbm as lgb
+import numpy as np
+import warnings
+import pickle
+import time
 
 # Data Creation Options - if you dont have the dataset you must
 # set both options to false to use the included pickle dataset
 # dataset can be found on Kaggle within the Home_Credit competetion
 create_new_data = True
 pickle_new_data = False
-save_classifier = True
+save_clf = True
 
 # creating/feteching the data
 features_train, features_test, labels_train, labels_test, features_test_final =\
-    data_creation_method(create_new_data, pickle_new_data, train_data_size = 10000)
+    data_creation_method(create_new_data, pickle_new_data, train_data_size=307511)
 
-# Tuned Support Vector Regression Classifier using GridSearchCV
-grid_values = {'tol': np.linspace(.0003, .001, 5), 'C': np.linspace(1, 3, 5), }
-clf = GridSearchCV(LinearSVC(), grid_values)
+# Simple LightGBM with preset settings
+lgb_train = lgb.Dataset(features_train, labels_train)
+lgb_eval = lgb.Dataset(features_test, labels_test, reference=lgb_train)
 
-print "********** Classifier Creation **********"
+# specify your configurations as a dict
+params = {'task': 'train', 'boosting_type': 'gbdt', 'objective': 'regression', 'metric': 'auc', 'num_leaves': 31,
+          'learning_rate': 0.05, 'feature_fraction': 0.9, 'bagging_fraction': 0.8, 'bagging_freq': 5, 'verbose': 0}
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
+print('Start training...')
+gbm = lgb.train(params, lgb_train, num_boost_round=1000, valid_sets=lgb_eval, early_stopping_rounds=50)
 
-    # select k-best feature selection
-    print "Selecting the 25-best Features"
-    selection = SelectKBest(k=10)
-    selection.fit(features_train, labels_train)
-    features_train_Kbest = selection.transform(features_train)
-    features_test_Kbest = selection.transform(features_test)
-    features_test_final_Kbest = selection.transform(features_test_final)
-    print " "
+print('Start predicting...')
+y_pred = gbm.predict(features_test, num_iteration=gbm.best_iteration)
+print('The Mean-Squared-Error of prediction is:', mean_squared_error(labels_test, y_pred) ** 0.5)
+print('Feature importances:', list(gbm.feature_importance())[0:25])
 
-    # Kfold Corss Validation
-    print "==Cross Validation on the training data=="
-    X = np.array(features_train_Kbest)
-    y = np.array(labels_train)
-    k_fold = StratifiedKFold(n_splits=10, shuffle=True)
-    cv = cross_validate(clf, X, y, cv=k_fold, n_jobs=1, scoring=['accuracy'])
-    print "Cross Validation Accuracy test:", "%.2f" % (100 * np.mean(cv['test_accuracy']))
-    print " "
+# save classifier to file
+if save_clf == True:
+    try:
+        save_classifier(clf)
+    except:
+        try:
+            save_classifier(gbm)
+        except:
+            gbm.save_model('/Users/mrciolino/Documents/Documents/PythonLearning/Kaggle Competetions/Home Credit/LightGBM.txt')
 
-    # Predicting the Testing Data
-    clf.fit(features_train_Kbest, labels_train)
-    pred = clf.predict(features_test_Kbest)
-    print "Parameters for the Classifier"
-    print clf.best_estimator_
-    print " "
-    print "Classification Report"
-    print classification_report(pred, labels_test)
-
-
-    if save_classifier == True:
-        clf_pickle_file = '/Users/mrciolino/Documents/Documents/PythonLearning/Kaggle Competetions/Home Credit/pickled_clf.pickle'
-        save_clf_data_to_pickle(clf_pickle_file, clf)
+create_submission(y_pred)
